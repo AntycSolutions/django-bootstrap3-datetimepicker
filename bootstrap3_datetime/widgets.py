9277b1d0dfabf55ccc75b477765d406d037c46cc
Solanar <and_555@hotmail.com>
2017-10-19 22:23:24 -0600

    django 1.11 compat

--- bootstrap3_datetime/widgets.py
+++ bootstrap3_datetime/widgets.py
@@ -127,7 +127,14 @@ class DateTimePicker(DateTimeInput):
     def render(self, name, value, attrs=None):
         if value is None:
             value = ''
-        input_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        try:
+            input_attrs = self.build_attrs(
+                attrs, type=self.input_type, name=name
+            )
+        except TypeError:
+            input_attrs = self.build_attrs(
+                attrs, extra_attrs={'type': self.input_type, 'name': name}
+            )
         if value != '':
             # Only add the 'value' attribute if a value is non-empty.
             input_attrs['value'] = force_text(self._format_value(value))
